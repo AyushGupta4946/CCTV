564b6e1df1b61bf8ac1c3775a2e329eb01d70cf4
Ayush Gupta <aagm0404@gmail.com>
2024-01-05 14:05:23 +0530

    added dark mode

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar")
 const link = document.querySelector(".link")
 const menuBtnIcon = document.querySelector(".fa-solid")
-
+const themeBtn = document.querySelector(".theme__button")
 
 hamburger.addEventListener("click", (e) => {
     navbar.classList.toggle("open")
@@ -14,4 +14,20 @@ hamburger.addEventListener("click", (e) => {
 navbar.addEventListener("click", (e) => {
     navbar.classList.remove("open");
     menuBtnIcon.setAttribute("class", "fa-solid fa-bars")
+})
+
+// theme toggler
+
+themeBtn.addEventListener('click', (e) => {
+    const body = document.body;
+
+    // Toggle the 'dark-mode' class on the body
+    body.classList.toggle('dark-mode');
+
+    // Check if 'dark-mode' is applied, and set the appropriate text on the button
+    const isDarkMode = body.classList.contains('dark-mode');
+    const buttonText = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+
+    // Update the button text
+    // document.querySelector('button').textContent = buttonText;
 })
